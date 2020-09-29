fd490ff131e5779d1d3e3daf635c0d561746de3e
Joshua walker <josh.d.walker@me.com>
2020-09-29 21:35:29 +0100

    test url firebase

--- src/js/app.js
+++ src/js/app.js
@@ -18,7 +18,7 @@ import * as firebase from 'firebase'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
   apiKey: "AIzaSyDItGG1opisGpeiVW-u18Ao4qy0ctFRQ1k",
-  authDomain: "odyssey-65e36.firebaseapp.com",
+  authDomain: "tlclassless.github.io/Odyssey/",
   databaseURL: "https://odyssey-65e36.firebaseio.com",
   projectId: "odyssey-65e36",
   storageBucket: "odyssey-65e36.appspot.com",
