f663ebd28f2950b12aa968cafdf59fd223acc937
Joshua walker <josh.d.walker@me.com>
2020-10-26 17:56:57 +0000

    uodate devtools

--- src/components/devtools.svelte
+++ src/components/devtools.svelte
@@ -1,4 +1,4 @@
-<Popup class="devtools-popup" opened={devtoolState} onPopupClosed={() => devtoolState = false}>
+<Popup id="devtools" >
   <Page>
     <Navbar title="DevTools">
       <NavRight>
@@ -6,14 +6,59 @@
       </NavRight>
     </Navbar>
     <Block>
-      <List inset>
 
-      
-        <ListItem>
-          <Logout></Logout>
-        </ListItem>
-    
+
+    <List mediaList inset>
+
+      <ListItem title="Geo Location" badge="{navigator.geolocation}" badgeColor="red" ></ListItem>
+
+      <ListItem title="Mapbox" badge="Offline" badgeColor="red" ></ListItem>
+
+      <ListItem title="Internet" badge="{navigator.onLine}" badgeColor="red" ></ListItem>
+
+      <ListItem title="Localstorage" badge="true" badgeColor="red" ></ListItem>
+
+      <ListItem title="User Status" badge="{navigator.onLine}" badgeColor="red" ></ListItem>
+
+      <ListItem title="User Agent" text="{navigator.userAgent}" ></ListItem>
+
+      <ListItem title="Cookies" text="{navigator.cookieEnabled}" ></ListItem>
+
+      <ListItem title="Language" text="{navigator.language}" ></ListItem>
+
+
+      <ListItem title="Host" text="{location.host}" ></ListItem>
+
+      <ListItem title="Hostname" text="{location.hostname}" ></ListItem>
+
+      <ListItem title="Href" text="{location.href}" ></ListItem>
+
+      <ListItem title="Origin" text="{location.origin}" ></ListItem>
+
+      <ListItem title="Pathname" text="{location.pathname}" ></ListItem>
+
+      <ListItem title="Port" text="{location.port}" ></ListItem>
+
+      <ListItem title="Protocol" text="{location.protocol}" ></ListItem>
+
+      <ListItem title="Search Query" text="{location.search}" ></ListItem>
+
+
+      <ListItem title="Screen Height" text="{screen.availHeight}" ></ListItem>
+
+      <ListItem title="Screen Width" text="{screen.availWidth}" ></ListItem>
+
+      <ListItem title="Window Height" text="{screen.height}" ></ListItem>
+
+      <ListItem title="Window Width" text="{screen.width}" ></ListItem>
+
+      <ListItem title="Color Resolution" text="{screen.pixelDepth}" ></ListItem>
+
+      <ListItem title="Color Depth" text="{screen.colorDepth}" ></ListItem>
+
+
     </List>
+
     </Block>
   </Page>
 </Popup>
@@ -23,6 +68,7 @@
 </style>
 
 <script>
+
 
 import {
     Page,
@@ -36,9 +82,5 @@ import {
     ListInput,
     Toggle
   } from 'framework7-svelte';
-
-  import Logout from '../components/logout.svelte';
-
-  var devtoolState = false;
 
 </script>
