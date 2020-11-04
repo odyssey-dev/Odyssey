55c99a392863b1b3b59f3a2e7b04e78a6f6b98bc
Ben <classle55sc2@gmail.com>
2020-11-04 13:01:45 +0000

    Created Bottom Toolbar

---
+++ src/components/toolbar/toolbar.svelte
@@ -0,0 +1,23 @@
+  <Tabs>
+    <Tab id="tab-1" class="page-content" tabActive>
+
+    </Tab>
+    <Tab id="tab-2" class="page-content">
+
+    </Tab>
+    <Tab id="tab-3" class="page-content">
+
+    </Tab>
+  </Tabs>
+
+  <Toolbar tabbar labels position={isBottom ? 'bottom' : 'top'}>
+    <Link tabLink="#tab-1" tabLinkActive text="Tab 1" iconIos="f7:envelope_fill" iconAurora="f7:envelope_fill" iconMd="material:email" />
+    <Link tabLink="#tab-2" text="Tab 2" iconIos="f7:calendar_fill" iconAurora="f7:calendar_fill" iconMd="material:today" />
+    <Link tabLink="#tab-3" text="Tab 3" iconIos="f7:cloud_upload_fill" iconAurora="f7:cloud_upload_fill" iconMd="material:file_upload" />
+  </Toolbar>
+
+  <script>
+    import {Page, Navbar, Toolbar, Link, BlockTitle, Block, NavRight, Tabs, Tab } from 'framework7-svelte';
+  
+    let isBottom = true;
+  </script>
