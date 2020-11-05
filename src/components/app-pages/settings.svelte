763c918ea7d34ea6120c930be3218c35d21f835a
Ben <classle55sc2@gmail.com>
2020-11-05 15:34:00 +0000

    Added Settings to Settings Page, fixed bug

---
+++ src/components/app-pages/settings.svelte
@@ -0,0 +1,160 @@
+<h1> Settings </h1>
+<List inset>
+
+    <h3>
+      General Settings
+    </h3>
+
+  <ListItem
+    title="Sounds"
+    >
+    <span slot="after">
+    <Toggle />
+    </span>
+  </ListItem>
+
+  <ListItem
+    title="Vibrations"
+    >
+    <span slot="after">
+    <Toggle />
+    </span>
+  </ListItem>
+
+  <ListItem
+  title="Animations"
+  >
+  <span slot="after">
+  <Toggle />
+  </span>
+</ListItem>
+
+  <ListItem
+    title="Monthly Challenges"
+    >
+    <span slot="after">
+    <Toggle />
+    </span>
+  </ListItem>
+
+  <ListItem
+    title="Email me Events/Updates"
+    >
+    <span slot="after">
+    <Toggle />
+    </span>
+  </ListItem>
+
+  <ListItem
+  title="Dark Mode"
+  >
+  <span slot="after">
+    <Toggle />
+  </span>
+</ListItem>
+
+</List>
+
+<List inset>
+
+    <h3>
+      Push Notifications
+    </h3>
+
+  <ListItem
+    title="On New Badge"
+    >
+    <span slot="after">
+      <Toggle />
+    </span>
+  </ListItem>
+
+  <ListItem
+    title="On Party Join"
+    >
+    <span slot="after">
+      <Toggle />
+    </span>
+  </ListItem>
+</List>
+
+<List inset>
+
+  <h3>
+    Language Settings
+  </h3>
+
+  <ListInput
+    label="Select Language"
+    type="select"
+    >
+    <option>English</option>
+    <option>Scots</option>
+    <option>Welsh</option>
+  </ListInput>
+
+</List>
+
+<List inset>
+
+  <h3>
+    Account Settings
+  </h3>
+
+  <ListInput
+    label="Select Home"
+    type="select"
+    >
+    <option>England</option>
+    <option>Northern Ireland</option>
+    <option>Scotland</option>
+    <option>Wales</option>
+  </ListInput>
+
+  <ListItem
+    title="Allow Location"
+    >
+    <span slot="after">
+      <Toggle />
+    </span>
+  </ListItem>
+
+    <ListItem>
+      <Link popupOpen="#devtools">Devtools</Link>
+    </ListItem>
+
+  <ListItem>
+    <Logout></Logout>
+  </ListItem>
+
+</List>
+
+<style>
+  h1 {
+    display: flex;
+    align-self: center;
+    margin-bottom: 0;
+    font-size: 16pt;
+    height: auto;
+  }
+
+</style>
+
+<script>
+  import {
+    Page,
+    Navbar,
+    Link,
+    List,
+    ListInput,
+    ListItem,
+    Toggle,
+    BlockTitle,
+    Row,
+    Button,
+    Range,
+    Block
+  } from 'framework7-svelte';
+
+  import Logout from "../logout.svelte";
+</script>
--- src/components/toolbar/toolbar.svelte
+++ src/components/toolbar/toolbar.svelte
@@ -1,20 +1,28 @@
   <Tabs>
+
     <Tab id="tab-1" class="page-content" tabActive>
 
     </Tab>
+
     <Tab id="tab-2" class="page-content">
 
     </Tab>
+
     <Tab id="tab-3" class="page-content">
 
     </Tab>
+
+    <Tab id="tab-4" class="page-content">
+
+    </Tab>
+
   </Tabs>
 
   <Toolbar tabbar labels position={isBottom ? 'bottom' : 'top'}>
     <Link tabLink="#tab-1" tabLinkActive text="Tab 1" iconIos="f7:envelope_fill" iconAurora="f7:envelope_fill" iconMd="material:email" />
     <Link tabLink="#tab-2" text="Tab 2" iconIos="f7:calendar_fill" iconAurora="f7:calendar_fill" iconMd="material:today" />
     <Link tabLink="#tab-3" text="Tab 3" iconIos="f7:cloud_upload_fill" iconAurora="f7:cloud_upload_fill" iconMd="material:file_upload" />
-    <Link tabLink="#tab-3" text="Tab 4" iconIos="f7:settings_fill" iconAurora="f7:settings_fill" iconMd="material:settings" />
+    <Link tabLink="#tab-4" text="Tab 4" iconIos="f7:settings_fill" iconAurora="f7:settings_fill" iconMd="material:settings" />
   </Toolbar>
 
   <script>
