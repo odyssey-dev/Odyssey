e89c972ae850a6d7cce69e9992dc75b9d3b32337
Ben <classle55sc2@gmail.com>
2020-11-05 15:29:22 +0000

    Added Pages for Toolbar (not linked)
    Will link in next commit

--- src/components/toolbar/toolbar.svelte
+++ src/components/toolbar/toolbar.svelte
@@ -14,6 +14,7 @@
     <Link tabLink="#tab-1" tabLinkActive text="Tab 1" iconIos="f7:envelope_fill" iconAurora="f7:envelope_fill" iconMd="material:email" />
     <Link tabLink="#tab-2" text="Tab 2" iconIos="f7:calendar_fill" iconAurora="f7:calendar_fill" iconMd="material:today" />
     <Link tabLink="#tab-3" text="Tab 3" iconIos="f7:cloud_upload_fill" iconAurora="f7:cloud_upload_fill" iconMd="material:file_upload" />
+    <Link tabLink="#tab-3" text="Tab 4" iconIos="f7:settings_fill" iconAurora="f7:settings_fill" iconMd="material:settings" />
   </Toolbar>
 
   <script>
