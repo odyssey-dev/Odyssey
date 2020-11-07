98dda8ca33d2312d57c235903ee0089683a3ec3c
Ben <classle55sc2@gmail.com>
2020-11-07 19:12:05 +0000

    changed icons

--- src/components/toolbar/toolbar.svelte
+++ src/components/toolbar/toolbar.svelte
@@ -19,10 +19,10 @@
   </Tabs>
 
   <Toolbar tabbar labels position={isBottom ? 'bottom' : 'top'}>
-    <Link tabLink="#tab-1" tabLinkActive text="Tab 1" iconIos="f7:envelope_fill" iconAurora="f7:envelope_fill" iconMd="material:email" />
-    <Link tabLink="#tab-2" text="Tab 2" iconIos="f7:calendar_fill" iconAurora="f7:calendar_fill" iconMd="material:today" />
-    <Link tabLink="#tab-3" text="Tab 3" iconIos="f7:cloud_upload_fill" iconAurora="f7:cloud_upload_fill" iconMd="material:file_upload" />
-    <Link tabLink="#tab-4" text="Tab 4" iconIos="f7:settings_fill" iconAurora="f7:settings_fill" iconMd="material:settings" />
+    <Link tabLink="#tab-1" text="Home" tabLinkActive iconIos="f7:house_fill" iconAurora="f7:house_fill" iconMd="f7:house_fill" />
+    <Link tabLink="#tab-2" text="Badges" iconIos="f7:star_circle_fill" iconAurora="f7:star_circle_fill" iconMd="f7:star_circle_fill" />
+    <Link tabLink="#tab-3" text="Profile" iconIos="f7:person_crop_circle_fill" iconAurora="f7:person_crop_circle_fill" iconMd="f7:person_crop_circle_fill" />
+    <Link tabLink="#tab-4" text="Settings" iconIos="f7:gear_alt_fill" iconAurora="f7:gear_alt_fill" iconMd="f7:gear_alt_fill" />
   </Toolbar>
 
   <script>
