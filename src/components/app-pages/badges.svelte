f78d8ea703316e38aa5aa096949ef3954185d66d
Ben <classle55sc2@gmail.com>
2020-11-14 23:35:06 +0000

    created and styled base badges page

--- src/components/app-pages/badges.svelte
+++ src/components/app-pages/badges.svelte
@@ -1,12 +1,94 @@
 <div class="card-parent">
     <div class="card-child">
-        
+      <div class="badge-card">
+        <div class="location-container">
+          <h3 class="location-title">Continent Badges</h3>
+          <span class="location-badges">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+          </span>
+        </div>
+
+        <hr size="1">
+
+        <div class="location-container">
+          <h3 class="location-title">Territory Badges</h3>
+          <span class="location-badges">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+          </span>
+        </div>
+
+        <hr size="1">
+
+        <div class="location-container">
+          <h3 class="location-title">Country Badges</h3>
+          <span class="location-badges">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+          </span>
+        </div>
+
+        <hr size="1">
+
+        <div class="location-container">
+          <h3 class="location-title">County Badges</h3>
+          <span class="location-badges">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+          </span>
+        </div>
+
+        <hr size="1">
+
+        <div class="location-container">
+          <h3 class="location-title">District Badges</h3>
+          <span class="location-badges">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+          </span>
+        </div>
+
+        <hr size="1">
+
+        <div class="location-container">
+          <h3 class="location-title">Challenge Badges</h3>
+          <span class="location-badges">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+          </span>
+        </div>
+
+        <hr size="1">
+
+        <div class="location-container">
+          <h3 class="location-title">Special Badges</h3>
+          <span class="location-badges">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+            <img src="./static/logo-variations/color-icon.svg" alt="badge">
+          </span>
+        </div>
+      </div>
     </div>
-    <div class="bottom-margin"></div>
 </div>
 
 
 <style>
+
   .card-parent {
     display: flex;
     position: fixed;
@@ -14,6 +96,14 @@
     height: 100%;
     z-index: 10;
     top: 0;
+  }
+
+  h3 {
+    margin: 0;
+    margin-bottom: 14px;
+    margin-left: calc(var(--f7-list-inset-side-margin) + var(--f7-safe-area-outer-left));
+    margin-right: calc(var(--f7-list-inset-side-margin) + var(--f7-safe-area-outer-right));
+    font-weight: 400;
   }
 
   .card-child {
@@ -29,4 +119,26 @@
     margin-bottom: calc(var(--f7-tabbar-labels-height) + 2%);
   }
 
+  .location-container {
+    margin: 24px 0;
+    margin-left: calc(var(--f7-list-inset-side-margin) + var(--f7-safe-area-outer-left));
+    margin-right: calc(var(--f7-list-inset-side-margin) + var(--f7-safe-area-outer-right));
+  }
+
+  .location-badges {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-left: calc(var(--f7-list-inset-side-margin) + var(--f7-safe-area-outer-left));
+    margin-right: calc(var(--f7-list-inset-side-margin) + var(--f7-safe-area-outer-right));
+  }
+
+  .badge-card {
+    padding-bottom: 10px;
+  }
+
+  .location-badges img {
+    height: 50px;
+    width: auto;
+  }
 </style>
