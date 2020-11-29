03e8a3b61880e3dccff931eb559b4ff9b54a0c17
Ben <classle55sc2@gmail.com>
2020-11-29 17:17:46 +0000

    Forgot to switch over sub-pages
    done now, also fixed formatting issue on settings

--- src/components/app-pages/badge-pages/badge-stack-template.svelte
+++ src/components/app-pages/badge-pages/badge-stack-template.svelte
@@ -1,44 +1,37 @@
 <script>
-
-  import Top from '../../profile';
+  import AppPage from '../app-pages';
   import { Toggle } from 'framework7-svelte';
-
 </script>
 
-<main>
-  <div class="card-parent">
-      <div class="card-child">
-          <Top></Top>
-          <div class="badge-content">
-            <span class="header">
-              <p>District Badges</p>
-              <span class="show-all">
-                <p>Show All</p>
-                <span class="divider"></span>
-                <Toggle />
-              </span>
-            </span>
+<AppPage>
+  <div class="badge-content">
+    <span class="header">
+      <p>District Badges</p>
+      <span class="show-all">
+        <p>Show All</p>
+        <span class="divider"></span>
+        <Toggle />
+      </span>
+    </span>
 
-            <div class="badges">
-              <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
-              <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
-              <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
-              <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
-              <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
-              <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
-              <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
-              <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
-              <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
-              <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
-              <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
-              <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
-              <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
-              <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
-            </div>
-          </div>
-      </div>
+    <div class="badges">
+      <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
+      <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
+      <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
+      <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
+      <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
+      <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
+      <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
+      <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
+      <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
+      <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
+      <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
+      <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
+      <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
+      <img src="../../../static/logo-variations/color-icon.svg" alt="badge">
+    </div>
   </div>
-</main>
+</AppPage>
 
 <style>
 
@@ -49,28 +42,6 @@
 p {
   font-weight: 700;
   font-size: 12pt;
-}
-
-.card-parent {
-  display: flex;
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  z-index: 10;
-  top: 0;
-}
-
-.card-child {
-  display: flex;
-  flex-direction: column;
-  background: #fff;
-  overflow-x: hidden;
-  overflow-y: hidden;
-  margin: 2%;
-  border-radius: 4px;
-  height: auto;
-  width: 100%;
-  margin-bottom: calc(var(--f7-tabbar-labels-height) + 2%);
 }
 
 .badge-content {
--- src/components/app-pages/settings.svelte
+++ src/components/app-pages/settings.svelte
@@ -1,4 +1,3 @@
-
 <AppPage>
 
 {#await profileSettings}
