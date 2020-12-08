723378d8f3f938b16bf6cb9549cb5389825d145d
Ben <classle55sc2@gmail.com>
2020-12-08 17:38:50 +0000

    Made modal look a little nicer

--- src/components/modal/achievement-get.svelte
+++ src/components/modal/achievement-get.svelte
@@ -1,20 +1,33 @@
 <style>
   .achievement-get { 
-      display: flex;
-      flex-direction: column;
+    display: flex;
+    flex-direction: column;
   }
   .badge-notif {
-      font-family: 'Rubik', sans-serif;
-      font-size: 18pt;
-      font-weight: 600;
-      margin-top: 0;
+    display: flex;
+    text-align: center;
+    font-family: 'Rubik', sans-serif;
+    font-size: 18pt;
+    font-weight: 600;
+    margin-top: 0;
   }
   .badge-name {
-      font-family: 'Roboto', sans-serif;
-      font-size: 12pt;
-      font-weight: 400;
-      margin-bottom: 0;
+    display: flex;
+    align-self: center;
+    font-family: 'Roboto', sans-serif;
+    font-size: 12pt;
+    font-weight: 400;
+    margin-bottom: 0;
   }
+  .badge-xp {
+    display: flex;
+    align-self: center;
+    font-family: 'Roboto', sans-serif;
+    font-size: 12pt;
+    font-weight: 600;
+    margin-top: 5px;
+  }
+
   #badge-img {
       height: 60%;
   }
@@ -25,8 +38,9 @@
     <Modal bind:this={modal}>
         <div class="achievement-get">
             <h1 class="badge-notif">Badge Unlocked!</h1>
-            <img id="badge-img" src="../static/badge/sample-badge.svg" alt="badge">
-            <h3 class="badge-name">Badge Name Here</h3>
+            <img id="badge-img" src="../../static/logo-variations/color-icon.svg" alt="badge">
+            <h3 class="badge-name">{badgeName}</h3>
+            <h3 class="badge-xp">{badgeXP}xp</h3>
         </div>
     </Modal>
 </main>
@@ -41,4 +55,8 @@
     export function playAudio() {
         badgeNotificationAudio.play();
     }
+
+    let badgeName = "Badge Name Here";
+    let badgeXP = 100;
+
 </script>
